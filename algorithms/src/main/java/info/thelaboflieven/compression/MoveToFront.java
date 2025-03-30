6496c003e3ac2e11883750192d73774ef48ca24e
denshade <lieven.vaneeckhaute@gmail.com>
2025-03-30 21:54:34 +0200

    refactor.

---
+++ algorithms/src/main/java/info/thelaboflieven/compression/MoveToFront.java
@@ -0,0 +1,25 @@
+package info.thelaboflieven.compression;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveToFront {
+
+    public static void encode(InputStream inputStream, OutputStream outputStream){
+        List<Byte> history = new ArrayList<>();
+        while(true) {
+            try {
+                int character = inputStream.read();
+                if (history.contains(character)) {
+
+                }
+                outputStream.write();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
