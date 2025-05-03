743deb7638b7aa9a80caeac69f37a631a62b0624
denshade <lieven.vaneeckhaute@gmail.com>
2025-05-03 17:47:05 +0200

    refactor

--- algorithms/src/test/java/info/thelaboflieven/compression/prefixcodes/UnaryCodeTest.java
+++ algorithms/src/test/java/info/thelaboflieven/compression/prefixcodes/UnaryCodeTest.java
@@ -9,22 +9,26 @@ class UnaryCodeTest {
     @Test
     public void testCode() {
         var unaryCode = new UnaryCode();
-        assertEquals(unaryCode.encode(1, new BitSetStream()).getBitSet().get(0), true);
-        assertEquals(unaryCode.encode(1, new BitSetStream()).getBitSet().get(1), false);
-        assertEquals(unaryCode.encode(2, new BitSetStream()).getBitSet().get(0), true);
-        assertEquals(unaryCode.encode(2, new BitSetStream()).getBitSet().get(1), true);
-        assertEquals(unaryCode.encode(2, new BitSetStream()).getBitSet().get(2), false);
-        assertEquals(unaryCode.encode(3, new BitSetStream()).getBitSet().get(0), true);
-        assertEquals(unaryCode.encode(3, new BitSetStream()).getBitSet().get(1), true);
-        assertEquals(unaryCode.encode(3, new BitSetStream()).getBitSet().get(2), true);
-        assertEquals(unaryCode.encode(3, new BitSetStream()).getBitSet().get(3), false);
+        assertTrue(getEncode(unaryCode, 1).getBitSet().get(0));
+        assertFalse(getEncode(unaryCode, 1).getBitSet().get(1));
+        assertTrue(getEncode(unaryCode, 2).getBitSet().get(0));
+        assertTrue(getEncode(unaryCode, 2).getBitSet().get(1));
+        assertFalse(getEncode(unaryCode, 2).getBitSet().get(2));
+        assertTrue(getEncode(unaryCode, 3).getBitSet().get(0));
+        assertTrue(getEncode(unaryCode, 3).getBitSet().get(1));
+        assertTrue(getEncode(unaryCode, 3).getBitSet().get(2));
+        assertFalse(getEncode(unaryCode, 3).getBitSet().get(3));
+    }
+
+    private BitSetStream getEncode(UnaryCode unaryCode, int number) {
+        return unaryCode.encode(number, new BitSetStream());
     }
 
     @Test
     public void testDeCode() {
         var unaryCode = new UnaryCode();
-        assertEquals(1, unaryCode.decode(unaryCode.encode(1, new BitSetStream())));
-        assertEquals(3, unaryCode.decode(unaryCode.encode(3, new BitSetStream())));
+        assertEquals(1, unaryCode.decode(getEncode(unaryCode, 1)));
+        assertEquals(3, unaryCode.decode(getEncode(unaryCode, 3)));
 
     }
 
